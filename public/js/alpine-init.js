af238debeaccaf8b6017ff3a01ea3b4e6f318e42
Beau Nieuwveld <16423070+itsbe-au@users.noreply.github.com>
2025-09-05 17:06:59 +1000

    Great start

---
+++ public/js/alpine-init.js
@@ -0,0 +1,14 @@
+
+document.addEventListener("alpine:init", () => {
+  Alpine.store("wheel", {
+    game: null,
+    selectedItem: null,
+    isSpinning: false,
+    showResultModal: false,
+
+    closeModal() {
+      this.showResultModal = false;
+      this.selectedItem = null;
+    }
+  });
+});
